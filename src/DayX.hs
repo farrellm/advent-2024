228a3e724ddcf17b4b671cf22faaaa1e560e0fe0
Matthew Farrell <farrellm@alum.mit.edu>
2024-12-02 23:33:07 -0500

    day 2

---
+++ src/DayX.hs
@@ -0,0 +1,19 @@
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
+module DayX where
+
+import Advent
+import Data.Map.Strict qualified as M
+
+stuff :: Parser ()
+stuff = pure ()
+
+part1 :: IO ()
+part1 = do
+  ps <- unsafeParse (stuff `sepEndBy` newline) "data/testX.txt"
+  print ps
+
+part2 :: IO ()
+part2 = do
+  ps <- unsafeParse (stuff `sepEndBy` newline) "data/textX.txt"
+  print ps
